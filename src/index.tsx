0f0859841255721f6bcf7c5f1cc518bc627772db
Aurimas Šimkus <saurimas@hotmail.com>
2019-06-07 03:24:20 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import ReactDOM from "react-dom";
 
 import "normalize.css";
 
-import { App } from "./App";
+import { App } from "./app";
 
 ReactDOM.render(<App />, document.getElementById("root"));
