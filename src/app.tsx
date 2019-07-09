e33310e85d76c2107f7fd6a577ffec4e0c27f088
ktkaciovaite <KATK01@storebrand.com>
2019-07-09 09:40:08 +0300

    Extracted AppList to separate component

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 
 import { Menu } from "./components/menu/menu";
-import { Tile } from "./components/tile/tile";
-import { appList } from "./appData";
+import { AppList } from "./components/appList/appList";
 
 import "./app.less";
 
@@ -10,14 +9,7 @@ const App = () => {
     return (
         <React.Fragment>
             <Menu />
-            <div className="app-container">
-                <div className="tile-container">
-                    {appList.map((app, index) => {
-                        return <Tile key={index} className={app.className} title={app.title} description={app.description}
-                            url={app.url} buttonText={app.buttonText} />;
-                    })}
-                </div>
-            </div>
+            <AppList />
         </React.Fragment>
     );
 };
---
+++ src/components/appList/appList.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import { Tile } from "../tile/tile";
+import { appList } from "./appListData";
+
+import "./appList.less";
+
+const AppList = () => {
+    return (
+        <div className="app-list-container">
+            <div className="tile-container">
+                {appList.map((app, index) => {
+                    return <Tile key={index} className={app.className} title={app.title} description={app.description}
+                        url={app.url} buttonText={app.buttonText} />;
+                })}
+            </div>
+        </div>
+    );
+};
+
+export { AppList };
