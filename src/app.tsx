2abecbc230f0ee17eb32fb60a2b0073aa8c67f89
ktkaciovaite <KATK01@storebrand.com>
2019-07-10 10:21:55 +0300

    Styled login form

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
+import { Login } from "./components/login/login";
 import { Menu } from "./components/menu/menu";
 import { AppList } from "./components/appList/appList";
 
 import "./app.less";
 
 const App = () => {
+    const [user, setUser] = useState({ isLoggedIn: false, isVerified: false });
+
     return (
-        <React.Fragment>
-            <Menu />
-            <AppList />
-        </React.Fragment>
+        user.isLoggedIn ? (
+            <React.Fragment>
+                <Menu />
+                <AppList />
+            </React.Fragment>
+        ) :
+        <Login isVerified={user.isVerified} />
     );
 };
 
