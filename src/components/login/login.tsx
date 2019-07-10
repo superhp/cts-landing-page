09a41c3f3d36d6567790e64bca2f210d7ba4f65c
edgar447 <edgar.reis447@gmail.com>
2019-07-10 15:42:22 +0300

    Revert "Login integrated with authentication api"
    This reverts commit af2dee108bdfb418482078eb311b3ca7121dae21.

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -2,9 +2,8 @@ import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebookSquare, faGoogle } from "@fortawesome/free-brands-svg-icons";
 import { faCircleNotch } from "@fortawesome/free-solid-svg-icons";
-import axios from "axios";
+
 import "./login.less";
-const logo = require("../../../public/img/cognizant-logo.svg");
 
 interface ILoginProps {
     isVerified: boolean;
@@ -13,30 +12,19 @@ interface ILoginProps {
 const initialState = {
     isLoggedIn: false,
     isEmailLoading: false,
-    isEmailSent: false,
-    cognizantEmail: null
+    isEmailSent: false
 };
 
 const Login = (props: ILoginProps) => {
     const [state, setState] = useState(initialState);
 
-    const emailInputChangeListner = (event: React.FormEvent<HTMLInputElement>) => setState({ ...state, cognizantEmail: event.currentTarget.value });
-
-    const facebookClickHandler = (event: React.MouseEvent) => axios.get("https://auth.ctsbaltic.com/api/auth/signin/Facebook?returnUrl=https%3A%2F%2Fctsbaltic.com")
-        .then(() => setState({ ...state, isLoggedIn: true }))
-        .catch();
-
-    const googleClickHandler = (event: React.MouseEvent) => axios.get("https://auth.ctsbaltic.com/api/auth/signin/Google?returnUrl=https%3A%2F%2Fctsbaltic.com")
-        .then(() => setState({ ...state, isLoggedIn: true }))
-        .catch();
-
-    const emailButtonClickHandler = (event: React.MouseEvent) => axios.post("https://auth.ctsbaltic.com/api/verification/emailCode", { email: state.cognizantEmail })
-        .then(() => setState({ ...state, isEmailLoading: true, isEmailSent: false }))
-        .catch();
+    const facebookClickHandler = (event: React.MouseEvent) => setState({ ...state, isLoggedIn: true });
+    const googleClickHandler = (event: React.MouseEvent) => setState({ ...state, isLoggedIn: true });
+    const emailButtonClickHandler = (event: React.MouseEvent) => setState({ ...state, isEmailLoading: true, isEmailSent: false });
 
     return (
         <div className="login-container">
-            <img className="logo" src={logo} />
+            <img className="logo" src="../../../public/img/cognizant-logo.svg" />
             <div className="login">
                 {!state.isLoggedIn && (
                     <React.Fragment>
@@ -59,7 +47,7 @@ const Login = (props: ILoginProps) => {
                         <div className="login-title">Almost done...</div>
                         <div className="login-subtitle">Verify that you are Cognizant employee</div>
                         <div className="login-email-verification">
-                            <input className="login-email" type="email" placeholder="Enter your Cognizant email" onChange={emailInputChangeListner} />
+                            <input className="login-email" type="email" placeholder="Enter your Cognizant email" />
                             <button className="login-email-button" onClick={emailButtonClickHandler}>
                                 {state.isEmailLoading && <FontAwesomeIcon icon={faCircleNotch} spin={true} />}
                                 {state.isEmailSent ? "Email sent!" : "Send verification email"}
--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,20 +1,15 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
+
 import "./menu.less";
 
 const logo = require("../../../public/img/cognizant-logo.svg");
 
 const Menu = () => {
-
-    const logoutClickHandler = (event: React.MouseEvent) => axios.get("https://auth.ctsbaltic.com/api/auth/signiout")
-        .catch();
-
     return (
-
         <div className="menu-container">
             <img className="logo" src={logo} alt="Cognizant logo" />
             <div className="menu">
-                <button onClick={logoutClickHandler} className="menu-item">Logout</button>
+                <button className="menu-item">Logout</button>
             </div>
         </div>
     );
