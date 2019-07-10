8b3efa096c9fb35bcb2ccdb1e1ba030d52a09ea9
edgar447 <edgar.reis447@gmail.com>
2019-07-10 15:52:03 +0300

    login integrated with authentication api

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,15 +1,17 @@
 import React from "react";
-
+import axios from "axios";
 import "./menu.less";
 
 const logo = require("../../../public/img/cognizant-logo.svg");
+const logoutClickHandler = (event: React.MouseEvent) => axios.get("https://auth.ctsbaltic.com/api/auth/signiout")
+    .catch();
 
-const Menu = () => {
+    const Menu = () => {
     return (
         <div className="menu-container">
             <img className="logo" src={logo} alt="Cognizant logo" />
             <div className="menu">
-                <button className="menu-item">Logout</button>
+                <button onClick={logoutClickHandler} className="menu-item">Logout</button>
             </div>
         </div>
     );
