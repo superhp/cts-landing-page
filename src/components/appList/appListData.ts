7f835b4e0133123cb57cef5ef5d7ee94eb579c0c
ReisEdgar <edgar.reis447@gmail.com>
2019-09-10 15:33:26 +0300

    Update appListData.ts

--- src/components/appList/appListData.ts
+++ src/components/appList/appListData.ts
@@ -1,15 +1,15 @@
-const appList = [{
+const appList = [/*{
     title: "ShareCar",
     description: "Share you car with collegues on your way to work!",
     url: "https://sharecar.ctsbaltic.com",
     buttonText: "Go",
     className: "share-car"
-}, {
+},*/ {
     title: "Hugs",
     description: "Get reward for something good you have done today!",
     url: "https://hugs.ctsbaltic.com",
     buttonText: "Go",
-    className: "hugs"
+    className: "share-car"
 }/*,{
     title: "ParkingApp",
     description: "Get the parking place next to your office!",
